f8c91f80eea831040d5d6d53155b08e8b82c4dc8
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2017-09-25 00:56:24 +0200

    Refactored init for tri3 and mesh class; added Ixy

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # section-properties
 Cross section analysis
+
+# To do
+1) Principal axes
+2) Section Modulus (Z)
+3) Plastic Modulus (S)
+4) Shear Area
+5) Shear Centre
+6) Torsion Constant (J)
+7) Warping Constant (Iw)
+8) Radius Area Integral (?)
+9) Stress visualisation
+10) Improve meshing
--- mesh2D.py
+++ mesh2D.py
@@ -3,59 +3,60 @@ import femFunctions
 
 class tri3:
     '''
-    Three noded triangle, input a 3 x 2 numpy array containing the co-ordinates of the vertices
+    Three noded triangle, input a 3 x 2 array containing the co-ordinates of the vertices
     '''
 
     def __init__(self, vertices):
         self.xy = vertices # triangle vertex co-ordinates [3 x 2]
-        self.area = self._area()
-        self.Qx = self._Qx()
-        self.Qy = self._Qy()
-        self.centroid = self._centroid()
-        self.ixx = self._ixx()
-        self.iyy = self._iyy()
+        self.initialise()
 
-    def _area(self):
+    def initialise(self):
+        # compute properties of element
+        gp1 = femFunctions.gaussPoints(1) # Gauss point for 1 point Gaussian integration
+        gp3 = femFunctions.gaussPoints(3) # Gauss points for 3 point Gaussian integration
+        (N1, dN1, j1) = femFunctions.shapeFunction(np.transpose(self.xy), gp1) # shape functions etc. for 1 point Gaussian integration
+
+        self.area = self._area(gp1, j1)
+        self.Qx = self._Qx(gp1, N1, j1)
+        self.Qy = self._Qy(gp1, N1, j1)
+        self.centroid = self._centroid()
+        self.ixx = self._ixx(gp3)
+        self.iyy = self._iyy(gp3)
+        self.ixy = self._ixy(gp3)
+
+    def _area(self, gp, j):
         '''
         Area of element: integral of the determinant of the jacobian over the element
         '''
         # jacobian is constant for a tri3 element, therefore use 1 point Gaussian integration
-        gp = femFunctions.gaussPoints(1)
-        (N, dN, j) = femFunctions.shapeFunction(np.transpose(self.xy), gp)
         return gp[0] * j  # Gauss point weight * determinant of jacobian at Gauss point
 
-    def _Qx(self):
+    def _Qx(self, gp, N, j):
         '''
         First moment of area about the global x-axis:
             - integral of [N * y * det(J)] over element
         '''
         # N * y * det(J) is linear for a tri3 element, therefore use 1 point Gaussian integration
-        gp = femFunctions.gaussPoints(1)
-        (N, dN, j) = femFunctions.shapeFunction(np.transpose(self.xy), gp)
         return gp[0] * np.dot(np.transpose(N), self.xy[:,1]) * j
 
-    def _Qy(self):
+    def _Qy(self, gp, N, j):
         '''
         First moment of area about the global y-axis:
             - integral of [N * x * det(J)] over element
         '''
         # N * x * det(J) is linear for a tri3 element, therefore use 1 point Gaussian integration
-        gp = femFunctions.gaussPoints(1)
-        (N, dN, j) = femFunctions.shapeFunction(np.transpose(self.xy), gp)
         return gp[0] * np.dot(np.transpose(N), self.xy[:,0]) * j
 
     def _centroid(self):
         ''' Centroid of element '''
         return np.array([self.Qy / self.area, self.Qx / self.area])
 
-    def _ixx(self):
+    def _ixx(self, gps):
         '''
         Second moment of area about the global x-axis:
             - integral of [(N * y)^2 * det(J)] over element
         '''
         # (N * y)^2 * det(J) is quadratic in 2D over a tri3 element, therefore use 3 point Gaussian integration
-        gps = femFunctions.gaussPoints(3)
-
         # loop through each gauss point to evaluate the integral at each gauss point
         total = 0
         for gp in gps.T:
@@ -64,19 +65,31 @@ class tri3:
 
         return total
 
-    def _iyy(self):
+    def _iyy(self, gps):
         '''
         Second moment of area about the global y-axis:
             - integral of [(N * x)^2 * det(J)] over element
         '''
         # (N * x)^2 * det(J) is quadratic in 2D over a tri3 element, therefore use 3 point Gaussian integration
-        gps = femFunctions.gaussPoints(3)
-
         # loop through each gauss point to evaluate the integral at each gauss point
         total = 0
         for gp in gps.T:
             (N, dN, j) = femFunctions.shapeFunction(np.transpose(self.xy), gp)
             total += gp[0] * (np.dot(np.transpose(N), self.xy[:,0])) ** 2 * j
+
+        return total
+
+    def _ixy(self, gps):
+        '''
+        Product of inertia about the global xy-axis:
+            - integral of [N * x * N * y * det(J)] over element
+        '''
+        # N * x * N * y * det(J) is quadratic in 2D over a tri3 element, therefore use 3 point Gaussian integration
+        # loop through each gauss point to evaluate the integral at each gauss point
+        total = 0
+        for gp in gps.T:
+            (N, dN, j) = femFunctions.shapeFunction(np.transpose(self.xy), gp)
+            total += gp[0] * np.transpose(N).dot(self.xy[:,0]) * np.transpose(N).dot(self.xy[:,1]) * j
 
         return total
 
@@ -99,81 +112,31 @@ class triMesh:
             triElements.append(tri3(vertex))
 
         self.elements = triElements
-        self.area = self._area()
-        self.Qx = self._Qx()
-        self.Qy = self._Qy()
-        self.cx = self._cx()
-        self.cy = self._cy()
-        self.ixx_g = self._ixx_g()
-        self.ixx_c = self._ixx_c()
-        self.iyy_g = self._iyy_g()
-        self.iyy_c = self._iyy_c()
-        self.rx = self._rx()
-        self.ry = self._ry()
+        self.initialise()
 
-    def _area(self):
-        ''' Total area of the cross section'''
-        total = 0
+    def initialise(self):
+        # initialise variables
+        totalArea = totalQx = totalQy = totalIxx_g = totalIyy_g = totalIxy_g = 0
 
+        # loop through all elements
         for el in self.elements:
-            total += el.area
+            totalArea += el.area
+            totalQx += el.Qx
+            totalQy += el.Qy
+            totalIxx_g += el.ixx
+            totalIyy_g += el.iyy
+            totalIxy_g += el.ixy
 
-        return total
-
-    def _Qx(self):
-        ''' First moment of area about the global x-axis'''
-        total = 0
-
-        for el in self.elements:
-            total += el.Qx
-
-        return total
-
-    def _Qy(self):
-        ''' First moment of area about the global y-axis'''
-        total = 0
-
-        for el in self.elements:
-            total += el.Qy
-
-        return total
-
-    def _cx(self):
-        ''' Cross-section x-position of centroid'''
-        return self.Qy / self.area
-
-    def _cy(self):
-        ''' Cross-section y-position of centroid'''
-        return self.Qx / self.area
-
-    def _ixx_g(self):
-        ''' Second moment of area of about the global x-axis'''
-        total = 0
-
-        for el in self.elements:
-            total += el.ixx
-
-        return total
-
-    def _iyy_g(self):
-        ''' Second moment of area of about the global y-axis'''
-        total = 0
-
-        for el in self.elements:
-            total += el.iyy
-
-        return total
-
-    def _ixx_c(self):
-        ''' Second moment of area of about the centroidal x-axis'''
-        return self.ixx_g - self.Qx ** 2 / self.area
-
-    def _iyy_c(self):
-        ''' Second moment of area of about the centroidal y-axis'''
-        return self.iyy_g - self.Qy ** 2 / self.area
-
-    def _rx(self):
-        return (self.ixx_c / self.area) ** 0.5
-
-    def _ry(self):
-        return (self.iyy_c / self.area) ** 0.5
+        self.area = totalArea
+        self.Qx = totalQx
+        self.Qy = totalQy
+        self.cx = totalQy / totalArea
+        self.cy = totalQx / totalArea
+        self.ixx_g = totalIxx_g
+        self.iyy_g = totalIyy_g
+        self.ixy_g = totalIxy_g
+        self.ixx_c = totalIxx_g - totalQx ** 2 / totalArea
+        self.iyy_c = totalIyy_g - totalQy ** 2 / totalArea
+        self.ixy_c = totalIxy_g - totalQx * totalQy / totalArea
+        self.rx = (self.ixx_c / totalArea) ** 0.5
+        self.ry = (self.iyy_c / totalArea) ** 0.5
