c5f6f408b66dec142345afa806d96bf0ab59aa65
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2017-11-05 17:06:56 +0100

    Cleaning repo

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import sectionGenerator
 # ------------------------------------------------------------------------------
 # INPUT GEOMETRY:
 # ------------------------------------------------------------------------------
-# Cruciform section
-(points, facets, holes) = sectionGenerator.Cruciform(200, 200, 8, 12, 8)
+# RHS section
+(points, facets, holes) = sectionGenerator.RHS(200, 100, 9, 15, 8)
 maxSize = 2.5
 
 # ------------------------------------------------------------------------------
@@ -45,7 +45,7 @@ refinedMesh = (femFunctions.createMesh(shiftedPoints, facets, shiftedHoles,
     maxArea=maxSize))
 
  # create mesh2D object for warping properties (load mesh with geom. properties)
-meshWarping = mesh2D.triMesh(refinedMesh, nu=0, geometricMesh=meshGeometric)
+meshWarping = mesh2D.triMesh(refinedMesh, nu=0.0, geometricMesh=meshGeometric)
 
 # plot warping mesh
 meshWarping.contourPlot(plotTitle='Mesh for Warping Properties')
@@ -64,39 +64,40 @@ meshWarping.unitStress()
 (meshWarping.contourPlot(plotTitle='Centroids', principalAxis=True,
     centroids=True))
 
-# apply forces/moments
-(meshWarping.evaluateSectionStress(Nzz=0, Mxx=10e6, Myy=0, M11=0, M22=0,
-    Mzz=1e6, Vx=0, Vy=50e3))
+if False:
+    # apply forces/moments
+    (meshWarping.evaluateSectionStress(Nzz=50e3, Mxx=40e6, Myy=12.5e6,
+        M11=0, M22=0, Mzz=10e6, Vx=20e3, Vy=50e3))
 
-# 1. Axial
-meshWarping.contourPlot(z=meshWarping.axialStress, plotTitle='Axial Stress')
+    # 1. Axial
+    meshWarping.contourPlot(z=meshWarping.axialStress, plotTitle='Axial Stress')
 
-# 2. Bending
-meshWarping.contourPlot(z=meshWarping.bendingStress, plotTitle='Bending Stress')
+    # 2. Bending
+    meshWarping.contourPlot(z=meshWarping.bendingStress, plotTitle='Bending Stress')
 
-# 3. Torsion
-meshWarping.contourPlot(z=meshWarping.torsionStress, plotTitle='Torsion Stress')
-(meshWarping.quiverPlot(meshWarping.torsionStress_zx,
-    meshWarping.torsionStress_zy, plotTitle='Torsion Stress Vectors'))
+    # 3. Torsion
+    meshWarping.contourPlot(z=meshWarping.torsionStress, plotTitle='Torsion Stress')
+    (meshWarping.quiverPlot(meshWarping.torsionStress_zx,
+        meshWarping.torsionStress_zy, plotTitle='Torsion Stress Vectors'))
 
-# 4. Shear
-(meshWarping.contourPlot(z=meshWarping.shearStress_zx,
-    plotTitle='Transverse Shear (zx) Stress'))
-(meshWarping.contourPlot(z=meshWarping.shearStress_zy,
-    plotTitle='Transverse Shear (zy) Stress'))
-(meshWarping.contourPlot(z=meshWarping.shearStress,
-    plotTitle='Transverse Shear Stress'))
-(meshWarping.quiverPlot(meshWarping.shearStress_zx, meshWarping.shearStress_zy,
-    plotTitle='Transverse Shear Stress Vectors'))
+    # 4. Shear
+    (meshWarping.contourPlot(z=meshWarping.shearStress_zx,
+        plotTitle='Transverse Shear (zx) Stress'))
+    (meshWarping.contourPlot(z=meshWarping.shearStress_zy,
+        plotTitle='Transverse Shear (zy) Stress'))
+    (meshWarping.contourPlot(z=meshWarping.shearStress,
+        plotTitle='Transverse Shear Stress'))
+    (meshWarping.quiverPlot(meshWarping.shearStress_zx, meshWarping.shearStress_zy,
+        plotTitle='Transverse Shear Stress Vectors'))
 
-# 5a. Combined Normal Stress
-(meshWarping.contourPlot(z=meshWarping.sigma_zz,
-    plotTitle='Combined Normal Stress'))
+    # 5a. Combined Normal Stress
+    (meshWarping.contourPlot(z=meshWarping.sigma_zz,
+        plotTitle='Combined Normal Stress'))
 
-# 5b. Combined Shear Stress
-(meshWarping.quiverPlot(meshWarping.tau_zx, meshWarping.tau_zy,
-    plotTitle='Combined Shear Stress Vectors'))
-meshWarping.contourPlot(z=meshWarping.tau, plotTitle='Combined Shear Stress')
+    # 5b. Combined Shear Stress
+    (meshWarping.quiverPlot(meshWarping.tau_zx, meshWarping.tau_zy,
+        plotTitle='Combined Shear Stress Vectors'))
+    meshWarping.contourPlot(z=meshWarping.tau, plotTitle='Combined Shear Stress')
 
-# 6. von Mises
-meshWarping.contourPlot(z=meshWarping.vonMises, plotTitle='von Mises Stress')
+    # 6. von Mises
+    meshWarping.contourPlot(z=meshWarping.vonMises, plotTitle='von Mises Stress')
