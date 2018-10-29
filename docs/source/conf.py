51081a2e2897827b6e5b4359a3b13aa37ab5ce6b
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2018-10-29 23:12:05 +1100

    Mock meshpy in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
+autodoc_mock_imports = ["meshpy"]
 
 # -- Project information -----------------------------------------------------
 
@@ -60,7 +61,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
