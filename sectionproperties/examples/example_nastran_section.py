530d1904b6dd19524fb094b2dd3daeeb53e91193
David John Neiferd <13425967+JohnDN90@users.noreply.github.com>
2019-11-12 18:19:01 -0500

    Cleaned up Nastran section examples.
    Added documentation for new example.

--- sectionproperties/examples/example_nastran_section.py
+++ sectionproperties/examples/example_nastran_section.py
@@ -1,12 +1,12 @@
 import sectionproperties.pre.nastran_sections as nsections
 from sectionproperties.analysis.cross_section import CrossSection
 
-# create a 50 diameter circle discretised by 64 points
-geometry = nsections.BoxSection(4.0, 3.0, 0.5, 0.375)
+# create a HAT1 section
+geometry = nsections.HAT1Section(DIM1=4.0, DIM2=2.0, DIM3=1.5, DIM4=0.1875, DIM5=0.375)
 geometry.plot_geometry()  # plot the geometry
 
-# create a mesh with a mesh size of 2.5
-mesh = geometry.create_mesh(mesh_sizes=[0.01])
+# create a mesh with a maximum elemental area of 0.005
+mesh = geometry.create_mesh(mesh_sizes=[0.005])
 
 section = CrossSection(geometry, mesh)  # create a CrossSection object
 section.display_mesh_info()  # display the mesh information
