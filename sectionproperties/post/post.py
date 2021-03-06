9494d3f572439b0be0cbe1526179745b877b6d9b
Scott Barlow <31610422+Spectre5@users.noreply.github.com>
2021-03-06 13:44:05 -0800

    add support for axis_index

--- sectionproperties/post/post.py
+++ sectionproperties/post/post.py
@@ -5,7 +5,9 @@ import contextlib
 
 
 @contextlib.contextmanager
-def plotting_context(ax=None, pause=True, title='', filename='', render=True, **kwargs):
+def plotting_context(
+    ax=None, pause=True, title='', filename='', render=True, axis_index=None, **kwargs
+):
     """Executes code required to set up a matplotlib figure.
 
     :param ax: Axes object on which to plot
@@ -17,6 +19,10 @@ def plotting_context(ax=None, pause=True, title='', filename='', render=True, **
         used, the figure is closed after the file is saved.
     :param bool render: If set to False, the image is not popped up. This may be useful if the
         figure or axes will be embedded or further edited before being displayed.
+    :param axis_index: If more than 1 axes is created by subplot, then this is the axis to plot on.
+        This may be a tuple if a 2D array of plots is returned.  The default value of None will
+        select the top left plot.
+    :type axis_index: Union[None, int, Tuple(int)]
     :param \**kwargs: Passed to :func:`matplotlib.pyplot.subplots`
     """
     if filename:
@@ -30,6 +36,16 @@ def plotting_context(ax=None, pause=True, title='', filename='', render=True, **
             plt.ion()
         ax_supplied = False
         (fig, ax) = plt.subplots(**kwargs)
+        try:
+            if axis_index is None:
+                axis_index = (0,) * ax.ndim
+            ax = ax[axis_index]
+        except (AttributeError, TypeError):
+            pass  # only 1 axis, not an array
+        except IndexError as exc:
+            raise ValueError(
+                f'axis_index={axis_index} is not compatible with arguments to subplots: {kwargs}'
+            ) from exc
     else:
         fig = ax.get_figure()
         ax_supplied = True
@@ -37,6 +53,7 @@ def plotting_context(ax=None, pause=True, title='', filename='', render=True, **
             plt.ioff()
 
     yield fig, ax
+    ax.set_aspect('equal', anchor='C')
 
     if ax_supplied:
         # if an axis was supplied, don't continue with displaying or configuring the plot
@@ -44,11 +61,11 @@ def plotting_context(ax=None, pause=True, title='', filename='', render=True, **
 
     # if no axes was supplied, finish the plot and return the figure and axes
     ax.set_title(title)
-    ax.set_aspect('equal', anchor='C')
     plt.tight_layout()
+
     if filename:
         fig.savefig(filename, dpi=fig.dpi)
-        plt.close(fig)
+        plt.close(fig)  # close the figure to free the memory
         return  # if the figure was to be saved, then don't show it also
 
     if render:
