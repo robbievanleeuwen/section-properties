b3c487944b7bef2169ce88df9d43a43ff4ddef20
Scott Barlow <31610422+Spectre5@users.noreply.github.com>
2021-03-06 12:47:21 -0800

    add plotting_context

--- sectionproperties/post/post.py
+++ sectionproperties/post/post.py
@@ -1,40 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import contextlib
 
-def setup_plot(pause):
+
+@contextlib.contextmanager
+def plotting_context(ax=None, pause=False, title='', filename='', render=True):
     """Executes code required to set up a matplotlib figure.
-
-    :param bool pause: If set to true, the figure pauses the script until the window is closed. If
-        set to false, the script continues immediately after the window is rendered.
-    """
-
-    if not pause:
-        plt.ion()
-        plt.show()
-    else:
-        plt.ioff()
-
-
-def finish_plot(ax, pause, title=''):
-    """Executes code required to finish a matplotlib figure.
 
     :param ax: Axes object on which to plot
     :type ax: :class:`matplotlib.axes.Axes`
     :param bool pause: If set to true, the figure pauses the script until the window is closed. If
         set to false, the script continues immediately after the window is rendered.
     :param string title: Plot title
+    :param string filename: Pass a non-empty string or path to save the image as. If this option is
+        used, the figure is closed after the file is saved.
+    :param bool render: If set to False, the image is not popped up. This may be useful if the
+        figure or axes will be embedded or further edited before being displayed.
     """
+    if ax is None:
+        ax_supplied = False
+        (fig, ax) = plt.subplots()
+        if not render:
+            plt.ioff()
+        elif pause:
+            plt.ioff()
+        else:
+            plt.ion()
+    else:
+        fig = ax.get_figure()
+        ax_supplied = True
+        if not render:
+            plt.ioff()
 
+    yield fig, ax
+
+    if ax_supplied:
+        # if an axis was supplied, don't continue with displaying or configuring the plot
+        return
+
+    # if no axes was supplied, finish the plot and return the figure and axes
     ax.set_title(title)
     ax.set_aspect('equal', anchor='C')
     plt.tight_layout()
+    if filename:
+        fig.savefig(filename, dpi=fig.dpi)
+        plt.close(fig)
+        return  # if the figure was to be saved, then don't show it also
 
-    if pause:
-        plt.show()
-    else:
-        plt.draw()
-        plt.pause(0.001)
+    if render:
+        if pause:
+            plt.show()
+        else:
+            plt.draw()
+            plt.pause(0.001)
 
 
 def draw_principal_axis(ax, phi, cx, cy):
