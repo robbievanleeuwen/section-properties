1725becf41319b57ff667b7e710f9fe79a4d6483
Connor Ferster <connorferster@gmail.com>
2021-06-06 18:25:28 -0700

    Feature: Added Geometry.assign_control_point() and casually tested control point persistence with geometry operations

--- sectionproperties/tests/test_sections.py
+++ sectionproperties/tests/test_sections.py
@@ -71,7 +71,7 @@ def test_geometry_from_points():
         [11, 7],
     ]
     holes = [[0, 6], [0, -6]]
-    new_geom = Geometry.from_points(points, facets, holes, control_point=[])
+    new_geom = Geometry.from_points(points, facets, holes, control_points=[])
     assert (
         new_geom.geom.wkt
         == 'POLYGON ((-6 10, 6 10, 6 -10, -6 -10, -6 10), (-4 8, -4 4, 4 4, 4 8, -4 8), (-4 -8, 4 -8, 4 -4, -4 -4, -4 -8))'
