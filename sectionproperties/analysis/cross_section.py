df0c1923306757f5cb9187119ddab67c3983676d
Colin Caprani <colin.caprani@monash.edu>
2021-10-13 13:15:34 +1100

    Add Mohrs to V2

--- sectionproperties/analysis/cross_section.py
+++ sectionproperties/analysis/cross_section.py
@@ -3840,30 +3840,30 @@ class StressPost:
         * :math:`V_{x} = 10` kN
         * :math:`V_{y} = 5` kN
         
-        at the point (10,88)
+        at the point (10,88.9)
 
         ::
 
             import sectionproperties.pre.sections as sections
-            from sectionproperties.analysis.cross_section import CrossSection
+            from sectionproperties.analysis.cross_section import Section
 
-            geometry = sections.AngleSection(d=150, b=90, t=12, r_r=10, r_t=5, n_r=8)
+            geometry = sections.angle_section(d=150, b=90, t=12, r_r=10, r_t=5, n_r=8)
             mesh = geometry.create_mesh(mesh_sizes=[2.5])
-            section = CrossSection(geometry, mesh)
+            section = Section(geometry, mesh)
 
             section.calculate_geometric_properties()
             section.calculate_warping_properties()
             stress_post = section.calculate_stress(
                 N=50e3, Mxx=-5e6, M22=2.5e6, Mzz=0.5e6, Vx=10e3, Vy=5e3
             )
-
-            stress_post.plot_mohrs_circles(10,88)
+            
+            stress_post.plot_mohrs_circles(10,88.9)
 
         ..  figure:: ../images/stress/mohrs_circles.png
             :align: center
             :scale: 75 %
 
-            Mohr's Circles of the 3D stress state at (10,88).
+            Mohr's Circles of the 3D stress state at (10,88.9).
         
         """
         
