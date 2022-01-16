40b1ac15ccc9bd66a400c5b95a8f2873d15adce3
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2022-01-16 20:00:54 +1100

    Bump version

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,11 @@
 Changelog:
 ==========
 
+v2.0.1:
+-------
+
+Fix issue with library module
+
 v2.0.0:
 -------
 
--- sectionproperties/__init__.py
+++ sectionproperties/__init__.py
@@ -1,9 +1,9 @@
 """
-A python package for the analysis of arbitrary cross-sections using the 
-finite element method written by Robbie van Leeuwen. 
+A python package for the analysis of arbitrary cross-sections using the
+finite element method written by Robbie van Leeuwen.
 
 sectionproperties can be used to determine section properties to be used
-in structural design and visualise cross-sectional stresses resulting 
+in structural design and visualise cross-sectional stresses resulting
 from combinations of applied forces and bending moments.
 """
-__version__ = "2.0.0"
+__version__ = "2.0.1"
