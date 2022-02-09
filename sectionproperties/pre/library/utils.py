8c788dd2ea53036f066b4103e591baa112cc225f
zmpulse <mathieu.bargues@gmail.com>
2022-02-09 16:24:10 +0100

    Update utils.py

--- sectionproperties/pre/library/utils.py
+++ sectionproperties/pre/library/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def draw_radius(pt: list, r: float, theta: float, n, ccw: bool = True):
+def draw_radius(pt: list, r: float, theta: float, n: int, ccw: bool = True):
     """Adds a quarter radius of points to the points list - centered at point *pt*, with radius
     *r*, starting at angle *theta*, with *n* points. If r = 0, adds pt only.
 
