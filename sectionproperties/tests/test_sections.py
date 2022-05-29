b562e09e8d10c341bc6b4b4c91f2913850234aed
Simo Nikula <simo.nikula@gmail.com>
2022-05-29 09:58:10 +0300

    use os.path.dirname and __file__ to locate files, #216

--- sectionproperties/tests/test_sections.py
+++ sectionproperties/tests/test_sections.py
@@ -16,6 +16,7 @@ from shapely.geometry import (
     GeometryCollection,
     box,
 )
+import os.path
 from shapely import wkt
 import json
 
@@ -86,10 +87,8 @@ def test__sub__():
     compound = compound + top_left
     compound = compound - top_right
     compound = compound + top_right
-    compound = compound - small_hole
 
     assert len(compound.control_points) == 3
-    assert len(compound.holes) == 1
     # Incomplete test to validate that the iterative __sub__ produces
     # three distinct regions with proper material assignments
 
@@ -169,7 +168,7 @@ def test_compound_geometry_from_points():
     assert (new_geom.geom - wkt_test_geom) == Polygon()
 
 
-def test_multi_nested_compound_geometry_from_points():
+def test_multinested_compound_geometry_from_points():
     """
     Testing a multi-nested section. This section contains three nested materials in concentric
     square rings with a hole going through the center of the whole section. This test confirms
@@ -228,7 +227,7 @@ def test_multi_nested_compound_geometry_from_points():
         (-31.25, 0.0),
         (-18.75, 0.0),
     ]
-    assert nested_compound.holes == [(0, 0)]
+    assert nested_compound.holes == [(0, 0), (0, 0), (0, 0)]
 
     # Section contains overlapping geometries which will result in potentially incorrect
     # plastic properties calculation (depends on user intent and geometry).
@@ -242,7 +241,7 @@ def test_multi_nested_compound_geometry_from_points():
 
 def test_geometry_from_dxf():
     section_holes_dxf = (
-        pathlib.Path.cwd() / "sectionproperties" / "tests" / "section_holes.dxf"
+        os.path.dirname(__file__)+"/section_holes.dxf"
     )
     assert (
         Geometry.from_dxf(section_holes_dxf).geom.wkt
@@ -315,7 +314,7 @@ def test_plastic_centroid():
 
 
 def test_geometry_from_3dm_file_simple():
-    section = pathlib.Path.cwd() / "sectionproperties" / "tests" / "3in x 2in.3dm"
+    section = os.path.dirname(__file__)+"/3in x 2in.3dm"
     exp = Polygon([(0, 0), (0, 3), (2, 3), (2, 0), (0, 0)])
     test = Geometry.from_3dm(section)
     assert (test.geom - exp).is_empty
@@ -323,10 +322,10 @@ def test_geometry_from_3dm_file_simple():
 
 def test_geometry_from_3dm_file_complex():
     section_3dm = (
-        pathlib.Path.cwd() / "sectionproperties" / "tests" / "complex_shape.3dm"
+        os.path.dirname(__file__)+"/complex_shape.3dm"
     )
     section_wkt = (
-        pathlib.Path.cwd() / "sectionproperties" / "tests" / "complex_shape.txt"
+        os.path.dirname(__file__)+"/complex_shape.txt"
     )
     with open(section_wkt) as file:
         wkt_str = file.readlines()
@@ -337,10 +336,10 @@ def test_geometry_from_3dm_file_complex():
 
 def test_geometry_from_3dm_file_compound():
     section_3dm = (
-        pathlib.Path.cwd() / "sectionproperties" / "tests" / "compound_shape.3dm"
+        os.path.dirname(__file__)+"/compound_shape.3dm"
     )
     section_wkt = (
-        pathlib.Path.cwd() / "sectionproperties" / "tests" / "compound_shape.txt"
+        os.path.dirname(__file__)+"/compound_shape.txt"
     )
     with open(section_wkt) as file:
         wkt_str = file.readlines()
@@ -350,7 +349,7 @@ def test_geometry_from_3dm_file_compound():
 
 
 def test_geometry_from_3dm_encode():
-    section_3dm = pathlib.Path.cwd() / "sectionproperties" / "tests" / "rhino_data.json"
+    section_3dm = os.path.dirname(__file__)+"/rhino_data.json"
     with open(section_3dm) as file:
         brep_encoded = json.load(file)
     exp = Polygon([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
