e4f433c0d3023aabfaad6d9adda8d580193dd7aa
Connor Ferster <connorferster@gmail.com>
2022-06-02 23:01:07 -0700

    Added current failing example from #219 as test case for test_check_geometry_overlaps()

--- sectionproperties/tests/test_sections.py
+++ sectionproperties/tests/test_sections.py
@@ -423,6 +423,9 @@ def test_check_geometry_overlaps():
     small_sq = rectangular_section(d=100, b=75)
     small_hole = rectangular_section(d=40, b=30).align_center(small_sq)
 
+    rect = rectangular_section(d=50, b=50)
+    circ = circular_section(d=50, n=32).shift_section(x_offset=125, y_offset=25)
+
     assert check_geometry_overlaps([small_sq.geom, small_hole.geom]) == True
     assert check_geometry_overlaps([small_sq.geom, small_sq.geom]) == True
     assert (
@@ -436,4 +439,10 @@ def test_check_geometry_overlaps():
             [big_sq.geom, small_sq.shift_section(x_offset=200, y_offset=150).geom]
         )
         == True
+    )
+
+    assert (
+        check_geometry_overlaps(
+            [rect.geom, circ.geom]
+        ) == False
     )
