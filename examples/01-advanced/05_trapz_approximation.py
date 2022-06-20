36a6752c8bea75581f71f0e72a05fa8d3f80a363
Robbie van Leeuwen <robbie.vanleeuwen@gmail.com>
2022-06-20 21:45:32 +1000

    Minor edits

--- examples/01-advanced/05_trapz_approximation.py
+++ examples/01-advanced/05_trapz_approximation.py
@@ -22,8 +22,7 @@ and what might the expected error be?
 # Define the Imports
 # ==================
 # Here we bring in the primative section shapes and also the more generic
-# Shapely `Polygon` object. We also use the nifty `tqdm` package to provide
-# a progress bar on computations.
+# Shapely `Polygon` object.
 import numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon
@@ -38,8 +37,7 @@ from sectionproperties.analysis.section import Section
 # It's better to collect the relevant section property calculation in a
 # single function. We are only interested in the torsion constant, so this
 # is straightforward enough. `geom` is the Section Property Gemoetry object;
-# `ms` is the mesh size. The "cgs" solver helps avoid singular matrices due
-# to small angles.
+# `ms` is the mesh size.
 def get_section_j(geom, ms, plot_geom=False):
     geom.create_mesh(mesh_sizes=[ms])
     section = Section(geom)
@@ -59,7 +57,7 @@ def get_section_j(geom, ms, plot_geom=False):
 n = 100  # mesh density
 
 #%%
-# Create and Analyze the Section
+# Create and Analyse the Section
 # ==============================
 # This function accepts the width `b` and a slope `S` to create the trapezoid.
 # Since we are only interested in relative results, the nominal dimensions are
@@ -101,7 +99,7 @@ print(f"{b=}; {S=}; {jr=}; {jt=}; {jr/jt}")
 #%%
 # Create Loop Variables
 # =====================
-# # The slope `S` is 0 for a rectangle, and 1 for a triangle and is
+# The slope `S` is 0 for a rectangle, and 1 for a triangle and is
 # defined per the plot below. A range of `S`, between 0.0 and 1.0 and
 # a range of `b` are considered, between 1 and 10 here (but can be extended)
 b_list = np.logspace(0, np.log10(10), 10)
