0e8753a6c7c7a20728cf385d9adb62c9900cb4a1
Connor Ferster <38441489+connorferster@users.noreply.github.com>
2022-10-17 17:45:26 -0700

    Blackify

--- sectionproperties/tests/test_concrete_sections.py
+++ sectionproperties/tests/test_concrete_sections.py
@@ -141,21 +141,38 @@ def test_concrete_circular_section():
 def test_concrete_column_section():
 
     concrete = pre.Material(
-        name='Concrete', elastic_modulus=30.1e3, poissons_ratio=0.2, yield_strength=32,
-        density=2.4e-6, color='lightgrey'
+        name="Concrete",
+        elastic_modulus=30.1e3,
+        poissons_ratio=0.2,
+        yield_strength=32,
+        density=2.4e-6,
+        color="lightgrey",
     )
     steel = pre.Material(
-        name='Steel', elastic_modulus=200e3, poissons_ratio=0.3, yield_strength=500,
-        density=7.85e-6, color='grey'
+        name="Steel",
+        elastic_modulus=200e3,
+        poissons_ratio=0.3,
+        yield_strength=500,
+        density=7.85e-6,
+        color="grey",
     )
 
     geometry = cs.concrete_column_section(
-        b=300, d=600, dia_bar=40, bar_area=500, cover=40, n_bars_b=3, n_bars_d=6, 
-        conc_mat=concrete, steel_mat=steel, filled=False, n_circle=4
-    ) # NOTE: Bar diam and Bar area do not match. This is intentional.
+        b=300,
+        d=600,
+        dia_bar=40,
+        bar_area=500,
+        cover=40,
+        n_bars_b=3,
+        n_bars_d=6,
+        conc_mat=concrete,
+        steel_mat=steel,
+        filled=False,
+        n_circle=4,
+    )  # NOTE: Bar diam and Bar area do not match. This is intentional.
     geometry.create_mesh(mesh_sizes=[500])
 
-        # check geometry is created correctly
+    # check geometry is created correctly
     conc_area = 0
     steel_area = 0
 
@@ -170,24 +187,25 @@ def test_concrete_column_section():
             )
 
     net_area = 300 * 600
-    actual_steel_area = 14 * 500.   
-    
+    actual_steel_area = 14 * 500.0
+
     check.almost_equal(conc_area, net_area - actual_steel_area, rel=r_tol)
     check.almost_equal(steel_area, actual_steel_area, rel=r_tol)
 
     bar_centroids = [tuple(geom.geom.centroid.coords[0]) for geom in geometry.geoms[1:]]
 
     from collections import Counter
+
     x_coords = Counter(round(coord[0], 0) for coord in bar_centroids)
     y_coords = Counter(round(coord[1], 0) for coord in bar_centroids)
 
     # Validate that we have 14 bars with the correct x-coordinates
-    check.equal(x_coords.get(60) , 6)
+    check.equal(x_coords.get(60), 6)
     check.equal(x_coords.get(150), 2)
     check.equal(x_coords.get(240), 6)
 
     # Validate that we have 14 bars with the correct y-coordinates
-    check.equal(y_coords.get(60) , 3)
+    check.equal(y_coords.get(60), 3)
     check.equal(y_coords.get(156), 2)
     check.equal(y_coords.get(252), 2)
     check.equal(y_coords.get(348), 2)
@@ -198,15 +216,19 @@ def test_concrete_column_section():
 def test_add_bar():
     rect = ps.rectangular_section(b=400, d=600)
     steel = pre.Material(
-        name='Steel', elastic_modulus=200e3, poissons_ratio=0.3, yield_strength=500,
-        density=7.85e-6, color='grey'
+        name="Steel",
+        elastic_modulus=200e3,
+        poissons_ratio=0.3,
+        yield_strength=500,
+        density=7.85e-6,
+        color="grey",
     )
     rect = cs.add_bar(rect, area=500, x=100, y=100, material=steel)
     rect = cs.add_bar(rect, area=500, x=200, y=200, material=steel)
     rect_area = rect.geoms[0].geom.area
-    steel_area = 2 * 500.
-    check.almost_equal(rect_area, 400*600 - steel_area)
-    
+    steel_area = 2 * 500.0
+    check.almost_equal(rect_area, 400 * 600 - steel_area)
+
     bar_1 = rect.geoms[1]
     bar_2 = rect.geoms[2]
     check.almost_equal(bar_1.calculate_centroid(), (100, 100))
