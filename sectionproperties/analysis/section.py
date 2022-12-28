4bad00ede4bda70fd2494a8c186b7021a38dc6f1
Norman JN Richardson <normanrichardson@gmail.com>
2022-12-27 17:36:52 -0800

    Update STRtree api to shapely 2.0

--- sectionproperties/analysis/section.py
+++ sectionproperties/analysis/section.py
@@ -193,7 +193,6 @@ class Section:
             Polygon(self.geometry.mesh["vertices"][tri][0:3])
             for tri in self.geometry.mesh["triangles"]
         ]
-        self.poly_mesh_idx = dict((id(poly), i) for i, poly in enumerate(p_mesh))
         self.mesh_search_tree = STRtree(p_mesh)
 
         # initialise class storing section properties
@@ -2259,11 +2258,7 @@ class Section:
 
         for pt in pts:
             query_geom = Point(pt)
-            tri_ids = [
-                self.poly_mesh_idx[id(poly)]
-                for poly in self.mesh_search_tree.query(query_geom)  # TODO
-                if poly.intersects(query_geom)
-            ]
+            tri_ids = self.mesh_search_tree.query(query_geom, predicate="intersects" )
             if len(tri_ids) == 0:
                 sig = None
             elif len(tri_ids) == 1:
