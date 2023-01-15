a128e0db10bedb9caefa54f0d5e14d3d94776606
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2023-01-16 07:51:06 +1100

    Fix primitive_sections.py

--- src/sectionproperties/pre/library/primitive_sections.py
+++ src/sectionproperties/pre/library/primitive_sections.py
@@ -1,77 +1,84 @@
+"""Primitive sections library."""
+
+from __future__ import annotations
+
 import numpy as np
 from shapely import Polygon
-import sectionproperties.pre.geometry as geometry
-import sectionproperties.pre.pre as pre
+
+import sectionproperties.pre.geometry as geom
 from sectionproperties.pre.library.utils import draw_radius
+from sectionproperties.pre.pre import DEFAULT_MATERIAL, Material
 
 
 def rectangular_section(
-    b: float, d: float, material: pre.Material = pre.DEFAULT_MATERIAL
-) -> geometry.Geometry:
-    """Constructs a rectangular section with the bottom left corner at the origin *(0, 0)*, with
-    depth *d* and width *b*.
+    d: float,
+    b: float,
+    material: Material = DEFAULT_MATERIAL,
+) -> geom.Geometry:
+    r"""Constructs a rectangular section.
 
-    :param float d: Depth (y) of the rectangle
-    :param float b: Width (x) of the rectangle
-    :param Optional[sectionproperties.pre.pre.Material]: Material to associate with this geometry
+    Constructs a rectangular section with the bottom left corner at the origin
+    ``(0, 0)``, with depth ``d`` and width ``b``.
 
-    The following example creates a rectangular cross-section with a depth of 100 and width of 50,
-    and generates a mesh with a maximum triangular area of 5::
+    Args:
+        d: Depth (``y``) of the rectangle
+        b: Width (``x``) of the rectangle
+        material: Material to associate with this geometry
 
-        from sectionproperties.pre.library.primitive_sections import rectangular_section
+    Return:
+        Rectangular section geometry
 
-        geometry = rectangular_section(d=100, b=50)
-        geometry.create_mesh(mesh_sizes=[5])
+    Example:
+        The following example creates a rectangular cross-section with a depth of 100 mm
+        and width of 50 mm:
 
-    ..  figure:: ../images/sections/rectangle_geometry.png
-        :align: center
-        :scale: 75 %
+        .. plot::
+            :include-source: True
+            :caption: Rectangular section geometry
 
-        Rectangular section geometry.
+            from sectionproperties.pre.library import rectangular_section
 
-    ..  figure:: ../images/sections/rectangle_mesh.png
-        :align: center
-        :scale: 75 %
-
-        Mesh generated from the above geometry.
+            rectangular_section(d=100, b=50).plot_geometry()
     """
-    points = [[0, 0], [b, 0], [b, d], [0, d]]
+    points = [(0, 0), (b, 0), (b, d), (0, d)]
     rectangle = Polygon(points)
-    return geometry.Geometry(rectangle, material)
+
+    return geom.Geometry(geom=rectangle, material=material)
 
 
 def circular_section(
-    d: float, n: int, material: pre.Material = pre.DEFAULT_MATERIAL
-) -> geometry.Geometry:
-    """Constructs a solid circle centered at the origin *(0, 0)* with diameter *d* and using *n*
-    points to construct the circle.
+    d: float,
+    n: int,
+    material: Material = DEFAULT_MATERIAL,
+) -> geom.Geometry:
+    """Constructs a circular section.
 
-    :param float d: Diameter of the circle
-    :param int n: Number of points discretising the circle
-    :param Optional[sectionproperties.pre.pre.Material]: Material to associate with this geometry
+    Constructs a solid circle centered at the origin ``(0, 0)`` with diameter ``d``,
+    using ``n`` points to construct the circle.
 
-    The following example creates a circular geometry with a diameter of 50 with 64 points,
-    and generates a mesh with a maximum triangular area of 2.5::
+    Args:
+        d: Diameter of the circle
+        n: Number of points discretising the circle
+        material: Material to associate with this geometry
 
-        from sectionproperties.pre.library.primitive_sections import circular_section
+    Return:
+        Circular section geometry
 
-        geometry = circular_section(d=50, n=64)
-        geometry.create_mesh(mesh_sizes=[2.5])
+    Example:
+        The following example creates a circular geometry with a diameter of 50 mm with
+        64 points:
 
-    ..  figure:: ../images/sections/circle_geometry.png
-        :align: center
-        :scale: 75 %
+        .. plot::
+            :include-source: True
+            :caption: Circular section geometry
 
-        Circular section geometry.
+            from sectionproperties.pre.library import circular_section
 
-    ..  figure:: ../images/sections/circle_mesh.png
-        :align: center
-        :scale: 75 %
-
-        Mesh generated from the above geometry.
+            circular_section(d=50, n=64).plot_geometry()
     """
     x_off, y_off = (0, 0)
     points = []
+
     # loop through each point on the circle
     for i in range(n):
         # determine polar angle
@@ -82,43 +89,43 @@ def circular_section(
         y = 0.5 * d * np.sin(theta) + y_off
 
         # append the current point to the points list
-        points.append([x, y])
+        points.append((x, y))
 
     circle = Polygon(points)
-    return geometry.Geometry(circle, material)
+
+    return geom.Geometry(geom=circle, material=material)
 
 
 def circular_section_by_area(
-    area: float, n: int, material: pre.Material = pre.DEFAULT_MATERIAL
-) -> geometry.Geometry:
-    """Constructs a solid circle centered at the origin *(0, 0)* defined by its *area*,
-    using *n* points to construct the circle.
+    area: float,
+    n: int,
+    material: Material = DEFAULT_MATERIAL,
+) -> geom.Geometry:
+    r"""Constructs a circular section defined by its area.
 
-    :param float area: Area of the circle
-    :param int n: Number of points discretising the circle
-    :param Optional[sectionproperties.pre.pre.Material]: Material to associate with this geometry
+    Constructs a solid circle centered at the origin ``(0, 0`` defined by its ``area``,
+    using ``n`` points to construct the circle.
 
-    The following example creates a circular geometry with an area of 200 with 32 points,
-    and generates a mesh with a maximum triangular area of 5::
+    Args:
+        area: Area of the circle
+        n: Number of points discretising the circle
+        material: Material to associate with this geometry
 
-        from sectionproperties.pre.library.primitive_sections import circular_section_by_area
+    Return:
+        Circular section geometry
 
-        geometry = circular_section_by_area(area=310, n=32)
-        geometry.create_mesh(mesh_sizes=[5])
+    Example:
+        The following example creates a circular geometry with an area of 310
+        mm\ :sup`2` with 32 points:
 
-    ..  figure:: ../images/sections/circle_area_geometry.png
-        :align: center
-        :scale: 50 %
+        .. plot::
+            :include-source: True
+            :caption: Circular section geometry
 
-        Circular section by area geometry.
+            from sectionproperties.pre.library import circular_section_by_area
 
-    ..  figure:: ../images/sections/circle_area_mesh.png
-        :align: center
-        :scale: 50 %
-
-        Mesh generated from the above geometry.
+            circular_section_by_area(area=310, n=32).plot_geometry()
     """
-
     s = 2 * np.sqrt(area / n) * np.sqrt(np.tan(np.pi / n))
     a = s / (2 * np.tan(np.pi / n))
     d = np.sqrt(a * a + (0.5 * s) * (0.5 * s)) * 2
@@ -127,36 +134,37 @@ def circular_section_by_area(
 
 
 def elliptical_section(
-    d_y: float, d_x: float, n: int, material: pre.Material = pre.DEFAULT_MATERIAL
-) -> geometry.Geometry:
-    """Constructs a solid ellipse centered at the origin *(0, 0)* with vertical diameter *d_y* and
-    horizontal diameter *d_x*, using *n* points to construct the ellipse.
+    d_x: float,
+    d_y: float,
+    n: int,
+    material: Material = DEFAULT_MATERIAL,
+) -> geom.Geometry:
+    """Constructs an elliptical section.
 
-    :param float d_y: Diameter of the ellipse in the y-dimension
-    :param float d_x: Diameter of the ellipse in the x-dimension
-    :param int n: Number of points discretising the ellipse
-    :param Optional[sectionproperties.pre.pre.Material]: Material to associate with this geometry
+    Constructs a solid ellipse centered at the origin ``(0, 0)`` with horizontal
+    diameter ``d_x`` and vertical diameter *d_y*, using ``n`` points to construct the
+    ellipse.
 
-    The following example creates an elliptical cross-section with a vertical diameter of 25 and
-    horizontal diameter of 50, with 40 points, and generates a mesh with a maximum triangular area
-    of 1.0::
+    Args:
+        d_x: Diameter of the ellipse in the x-dimension
+        d_y: Diameter of the ellipse in the y-dimension
+        n: Number of points discretising the ellipse
+        material: Material to associate with this geometry
 
-        from sectionproperties.pre.library.primitive_sections import elliptical_section
+    Return:
+        Elliptical section geometry
 
-        geometry = elliptical_section(d_y=25, d_x=50, n=40)
-        geometry.create_mesh(mesh_sizes=[1.0])
+    Example:
+        The following example creates an elliptical cross-section with a horizontal
+        diameter of 50 mm and a vertical diameter of 25 mm, with 40 points:
 
-    ..  figure:: ../images/sections/ellipse_geometry.png
-        :align: center
-        :scale: 75 %
+        .. plot::
+            :include-source: True
+            :caption: Elliptical section geometry
 
-        Elliptical section geometry.
+            from sectionproperties.pre.library import elliptical_section
 
-    ..  figure:: ../images/sections/ellipse_mesh.png
-        :align: center
-        :scale: 75 %
-
-        Mesh generated from the above geometry.
+            elliptical_section(d_x=50, d_y=25, n=40).plot_geometry()
     """
     points = []
 
@@ -170,81 +178,82 @@ def elliptical_section(
         y = 0.5 * d_y * np.sin(theta)
 
         # append the current point to the points list
-        points.append([x, y])
+        points.append((x, y))
 
     ellipse = Polygon(points)
-    return geometry.Geometry(ellipse, material)
+
+    return geom.Geometry(geom=ellipse, material=material)
 
 
 def triangular_section(
-    b: float, h: float, material: pre.Material = pre.DEFAULT_MATERIAL
-) -> geometry.Geometry:
-    """Constructs a right angled triangle with points *(0, 0)*, *(b, 0)*, *(0, h)*.
+    b: float,
+    h: float,
+    material: Material = DEFAULT_MATERIAL,
+) -> geom.Geometry:
+    """Constructs a triangular section.
 
-    :param float b: Base length of triangle
-    :param float h: Height of triangle
-    :param Optional[sectionproperties.pre.pre.Material]: Material to associate with this geometry
+    Constructs a right angled triangle with points ``(0, 0)``, ``(b, 0)``, ``(0, h)``.
 
-    The following example creates a triangular cross-section with a base width of 10 and height of
-    10, and generates a mesh with a maximum triangular area of 0.5::
+    Args:
+        b: Base length of triangle
+        h: Height of triangle
+        material: Material to associate with this geometry
 
-        from sectionproperties.pre.library.primitive_sections import triangular_section
+    Return:
+        Triangular section geometry
 
-        geometry = triangular_section(b=10, h=10)
-        geometry.create_mesh(mesh_sizes=[0.5])
+    Example:
+        The following example creates a triangular cross-section with a base width of 10
+        mm and height of 10 mm:
 
-    ..  figure:: ../images/sections/triangle_geometry.png
-        :align: center
-        :scale: 40 %
+        .. plot::
+            :include-source: True
+            :caption: Triangular section geometry
 
-        Triangular section geometry.
+            from sectionproperties.pre.library import triangular_section
 
-    ..  figure:: ../images/sections/triangle_mesh.png
-        :align: center
-        :scale: 40 %
-
-        Mesh generated from the above geometry.
+            triangular_section(b=10, h=10).plot_geometry()
     """
-    points = [[0, 0], [b, 0], [0, h]]
+    points = [(0, 0), (b, 0), (0, h)]
     triangle = Polygon(points)
-    return geometry.Geometry(triangle, material)
+    return geom.Geometry(triangle, material)
 
 
 def triangular_radius_section(
-    b: float, n_r: float, material: pre.Material = pre.DEFAULT_MATERIAL
-) -> geometry.Geometry:
-    """Constructs a right angled isosceles triangle with points *(0, 0)*, *(b, 0)*, *(0, h)* and a
-    concave radius on the hypotenuse.
+    b: float,
+    n_r: int,
+    material: Material = DEFAULT_MATERIAL,
+) -> geom.Geometry:
+    """Constructs a triangular section with a radius.
 
-    :param float b: Base length of triangle
-    :param int n_r: Number of points discretising the radius
-    :param Optional[sectionproperties.pre.pre.Material]: Material to associate with this geometry
+    Constructs a right angled isosceles triangle with points``(0, 0)``, ``(b, 0)``,
+    ``(0, h)`` and a concave radius on the hypotenuse.
 
-    The following example creates a triangular radius cross-section with a base width of 6, using
-    *n_r* points to construct the radius, and generates a mesh with a maximum triangular area of
-    0.5::
+    Args:
+        b: Base length of triangle
+        n_r: Number of points discretising the radius
+        material: Material to associate with this geometry
 
-        from sectionproperties.pre.library.primitive_sections import triangular_radius_section
+    Return:
+        Triangular section geometry
 
-        geometry = triangular_radius_section(b=6, n_r=16)
-        geometry.create_mesh(mesh_sizes=[0.5])
+    Example:
+        The following example creates an isosceles triangular radius cross-section with
+        a base width of 6 mm, using 16 points to construct the radius:
 
-    ..  figure:: ../images/sections/triangle_radius_geometry.png
-        :align: center
-        :scale: 40 %
+        .. plot::
+            :include-source: True
+            :caption: Triangular section geometry
 
-        Triangular radius section geometry.
+            from sectionproperties.pre.library import triangular_radius_section
 
-    ..  figure:: ../images/sections/triangle_radius_mesh.png
-        :align: center
-        :scale: 40 %
-
-        Mesh generated from the above geometry.
+            triangular_radius_section(b=6, n_r=16).plot_geometry()
     """
-    points = [(0, 0)]
-    points += draw_radius(pt=[b, b], r=b, theta=3 * np.pi / 2, n=n_r, ccw=False)
+    points = [(0.0, 0.0)]
+    points += draw_radius(pt=(b, b), r=b, theta=3 * np.pi / 2, n=n_r, ccw=False)
     triangle = Polygon(points)
-    return geometry.Geometry(triangle, material)
+
+    return geom.Geometry(geom=triangle, material=material)
 
 
 def cruciform_section(
@@ -253,72 +262,72 @@ def cruciform_section(
     t: float,
     r: float,
     n_r: int,
-    material: pre.Material = pre.DEFAULT_MATERIAL,
-) -> geometry.Geometry:
-    """Constructs a cruciform section centered at the origin *(0, 0)*, with depth *d*, width *b*,
-    thickness *t* and root radius *r*, using *n_r* points to construct the root radius.
+    material: Material = DEFAULT_MATERIAL,
+) -> geom.Geometry:
+    """Constructs a cruciform section.
 
-    :param float d: Depth of the cruciform section
-    :param float b: Width of the cruciform section
-    :param float t: Thickness of the cruciform section
-    :param float r: Root radius of the cruciform section
-    :param int n_r: Number of points discretising the root radius
-    :param Optional[sectionproperties.pre.pre.Material]: Material to associate with this geometry
+    Constructs a cruciform section centered at the origin ``(0, 0)``, with depth ``d``,
+    width ``b``, thickness ``t`` and root radius ``r``, using ``n_r`` points to
+    construct the root radius.
 
-    The following example creates a cruciform section with a depth of 250, a width of 175, a
-    thickness of 12 and a root radius of 16, using 16 points to discretise the radius. A mesh is
-    generated with a maximum triangular area of 5.0::
+    Args:
+        d: Depth of the cruciform section
+        b: Width of the cruciform section
+        t: Thickness of the cruciform section
+        r: Root radius of the cruciform section
+        n_r: Number of points discretising the root radius
+        material: Material to associate with this geometry
 
-        from sectionproperties.pre.library.primitive_sections import cruciform_section
+    Return:
+        Cruciform section geometry
 
-        geometry = cruciform_section(d=250, b=175, t=12, r=16, n_r=16)
-        geometry.create_mesh(mesh_sizes=[5.0])
+    Example:
+        The following example creates a cruciform section with a depth of 250 mm, a
+        width of 175 mm, a thickness of 12 mm and a root radius of 16 mm, using 16
+        points to discretise the radius:
 
-    ..  figure:: ../images/sections/cruciform_geometry.png
-        :align: center
-        :scale: 75 %
+        .. plot::
+            :include-source: True
+            :caption: Cruciform section geometry
 
-        Cruciform section geometry.
+            from sectionproperties.pre.library import cruciform_section
 
-    ..  figure:: ../images/sections/cruciform_mesh.png
-        :align: center
-        :scale: 75 %
-
-        Mesh generated from the above geometry.
+            cruciform_section(d=250, b=175, t=12, r=16, n_r=16).plot_geometry()
     """
-    points = []
+    points: list[tuple[float, float]] = []
 
     # add first two points
-    points.append([-t * 0.5, -d * 0.5])
-    points.append([t * 0.5, -d * 0.5])
+    points.append((-t * 0.5, -d * 0.5))
+    points.append((t * 0.5, -d * 0.5))
 
     # construct the bottom right radius
-    pt = [0.5 * t + r, -0.5 * t - r]
-    points += draw_radius(pt, r, np.pi, n_r, False)
+    pt = 0.5 * t + r, -0.5 * t - r
+    points += draw_radius(pt=pt, r=r, theta=np.pi, n=n_r, ccw=False)
 
     # add the next two points
-    points.append([0.5 * b, -t * 0.5])
-    points.append([0.5 * b, t * 0.5])
+    points.append((0.5 * b, -t * 0.5))
+    points.append((0.5 * b, t * 0.5))
 
     # construct the top right radius
-    pt = [0.5 * t + r, 0.5 * t + r]
-    points += draw_radius(pt, r, 1.5 * np.pi, n_r, False)
+    pt = 0.5 * t + r, 0.5 * t + r
+    points += draw_radius(pt=pt, r=r, theta=1.5 * np.pi, n=n_r, ccw=False)
 
     # add the next two points
-    points.append([t * 0.5, 0.5 * d])
-    points.append([-t * 0.5, 0.5 * d])
+    points.append((t * 0.5, 0.5 * d))
+    points.append((-t * 0.5, 0.5 * d))
 
     # construct the top left radius
-    pt = [-0.5 * t - r, 0.5 * t + r]
-    points += draw_radius(pt, r, 0, n_r, False)
+    pt = -0.5 * t - r, 0.5 * t + r
+    points += draw_radius(pt=pt, r=r, theta=0, n=n_r, ccw=False)
 
     # add the next two points
-    points.append([-0.5 * b, t * 0.5])
-    points.append([-0.5 * b, -t * 0.5])
+    points.append((-0.5 * b, t * 0.5))
+    points.append((-0.5 * b, -t * 0.5))
 
     # construct the bottom left radius
-    pt = [-0.5 * t - r, -0.5 * t - r]
-    points += draw_radius(pt, r, 0.5 * np.pi, n_r, False)
+    pt = -0.5 * t - r, -0.5 * t - r
+    points += draw_radius(pt=pt, r=r, theta=0.5 * np.pi, n=n_r, ccw=False)
 
     polygon = Polygon(points)
-    return geometry.Geometry(polygon, material)
+
+    return geom.Geometry(geom=polygon, material=material)
