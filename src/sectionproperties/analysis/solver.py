29cdc1ce24586571231bfc95737f597315186615
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2023-01-19 16:32:05 +1100

    Fix solver.py

--- src/sectionproperties/analysis/solver.py
+++ src/sectionproperties/analysis/solver.py
@@ -1,31 +1,44 @@
-import time
+"""Methods used for solving linear systems and displaying info on tasks."""
+
+from __future__ import annotations
+
 import numpy as np
-from rich.progress import Progress, BarColumn, ProgressColumn, TextColumn, SpinnerColumn
+from rich.progress import (
+    BarColumn,
+    Progress,
+    ProgressColumn,
+    SpinnerColumn,
+    Task,
+    TextColumn,
+)
 from rich.table import Column
 from rich.text import Text
-from scipy.sparse import linalg
-from scipy.sparse.linalg import spsolve
+from scipy.sparse import csc_matrix, linalg
+from scipy.sparse.linalg import LinearOperator, spsolve
 
 
-def solve_cgs(k, f, m=None, tol=1e-5):
-    """Solves a linear system of equations (Ku = f) using the CGS iterative method.
+def solve_cgs(
+    k: csc_matrix,
+    f: np.ndarray,
+    m: LinearOperator | None = None,
+    tol: float = 1e-5,
+) -> np.ndarray:
+    """Solves a linear system using the CGS iterative method.
 
-    :param k: N x N matrix of the linear system
-    :type k: :class:`scipy.sparse.csc_matrix`
-    :param f: N x 1 right hand side of the linear system
-    :type f: :class:`numpy.ndarray`
-    :param float tol: Tolerance for the solver to achieve. The algorithm terminates when either
-        the relative or the absolute residual is below tol.
-    :param m: Preconditioner for the linear matrix approximating the inverse of k
-    :type m: :class:`scipy.linalg.LinearOperator`
+    Args:
+        k: ``N x N`` matrix of the linear system
+        f: ``N x 1`` right hand side of the linear system
+        tol: Tolerance for the solver to achieve. The algorithm terminates when either
+            the relative or the absolute residual is below ``tol``
+        m: Preconditioner for the linear matrix approximating the inverse of ``k``
 
-    :return: The solution vector to the linear system of equations
-    :rtype: :class:`numpy.ndarray`
+    Return:
+        The solution vector to the linear system of equations
 
-    :raises RuntimeError: If the CGS iterative method does not converge
+    Raises:
+        RuntimeError: If the CGS iterative method does not converge
     """
-
-    (u, info) = linalg.cgs(k, f, tol=tol, M=m)
+    u, info = linalg.cgs(A=k, b=f, tol=tol, M=m)
 
     if info != 0:
         raise RuntimeError("CGS iterative method did not converge.")
@@ -33,27 +46,29 @@ def solve_cgs(k, f, m=None, tol=1e-5):
     return u
 
 
-def solve_cgs_lagrange(k_lg, f, tol=1e-5, m=None):
-    """Solves a linear system of equations (Ku = f) using the CGS iterative method and the
-    Lagrangian multiplier method.
+def solve_cgs_lagrange(
+    k_lg: csc_matrix,
+    f: np.ndarray,
+    m: LinearOperator | None = None,
+    tol: float = 1e-5,
+) -> np.ndarray:
+    """Solves a linear system using the CGS iterative method (Lagrangian multiplier).
 
-    :param k: (N+1) x (N+1) Lagrangian multiplier matrix of the linear system
-    :type k: :class:`scipy.sparse.csc_matrix`
-    :param f: N x 1 right hand side of the linear system
-    :type f: :class:`numpy.ndarray`
-    :param float tol: Tolerance for the solver to achieve. The algorithm terminates when either
-        the relative or the absolute residual is below tol.
-    :param m: Preconditioner for the linear matrix approximating the inverse of k
-    :type m: :class:`scipy.linalg.LinearOperator`
+    Args:
+        k_lg: ``(N+1) x (N+1)`` Lagrangian multiplier matrix of the linear system
+        f:`` N x 1`` right hand side of the linear system
+        tol: Tolerance for the solver to achieve. The algorithm terminates when either
+            the relative or the absolute residual is below ``tol``
+        m: Preconditioner for the linear matrix approximating the inverse of ``k``
 
-    :return: The solution vector to the linear system of equations
-    :rtype: :class:`numpy.ndarray`
+    Return:
+        The solution vector to the linear system of equations
 
-    :raises RuntimeError: If the CGS iterative method does not converge or the error from the
-        Lagrangian multiplier method exceeds the tolerance
+    Raises:
+        RuntimeError: If the CGS iterative method does not converge or the error from
+            the Lagrangian multiplier method exceeds the tolerance
     """
-
-    (u, info) = linalg.cgs(k_lg, np.append(f, 0), tol=tol, M=m)
+    u, info = linalg.cgs(A=k_lg, b=np.append(f, 0), tol=tol, M=m)
 
     if info != 0:
         raise RuntimeError("CGS iterative method did not converge.")
@@ -62,50 +77,52 @@ def solve_cgs_lagrange(k_lg, f, tol=1e-5, m=None):
     err = u[-1] / max(np.absolute(u))
 
     if err > tol:
-        err = "Lagrangian multiplier method error exceeds tolerance."
-        raise RuntimeError(err)
+        raise RuntimeError("Lagrangian multiplier method error exceeds tolerance.")
 
     return u[:-1]
 
 
-def solve_direct(k, f):
-    """Solves a linear system of equations (Ku = f) using the direct solver method.
+def solve_direct(
+    k: csc_matrix,
+    f: np.ndarray,
+) -> np.ndarray:
+    """Solves a linear system using the direct solver method.
 
-    :param k: N x N matrix of the linear system
-    :type k: :class:`scipy.sparse.csc_matrix`
-    :param f: N x 1 right hand side of the linear system
-    :type f: :class:`numpy.ndarray`
+    Args:
+        k: ``N x N`` matrix of the linear system
+        f: ``N x 1`` right hand side of the linear system
 
-    :return: The solution vector to the linear system of equations
-    :rtype: :class:`numpy.ndarray`
+    Return:
+        The solution vector to the linear system of equations
     """
+    return spsolve(A=k, b=f)
 
-    return spsolve(k, f)
 
+def solve_direct_lagrange(
+    k_lg: csc_matrix,
+    f: np.ndarray,
+) -> np.ndarray:
+    """Solves a linear system using the direct solver method (Lagrangian multiplier).
 
-def solve_direct_lagrange(k_lg, f):
-    """Solves a linear system of equations (Ku = f) using the direct solver method and the
-    Lagrangian multiplier method.
+    Args:
+        k: ``(N+1) x (N+1)`` Lagrangian multiplier matrix of the linear system
+        f: ``N x 1`` right hand side of the linear system
 
-    :param k: (N+1) x (N+1) Lagrangian multiplier matrix of the linear system
-    :type k: :class:`scipy.sparse.csc_matrix`
-    :param f: N x 1 right hand side of the linear system
-    :type f: :class:`numpy.ndarray`
+    Return:
+        The solution vector to the linear system of equations
 
-    :return: The solution vector to the linear system of equations
-    :rtype: :class:`numpy.ndarray`
-
-    :raises RuntimeError: If the Lagrangian multiplier method exceeds a tolerance of 1e-5
+    Raises:
+        RuntimeError: If the Lagrangian multiplier method exceeds a tolerance of
+            ``1e-5``
     """
-
-    u = spsolve(k_lg, np.append(f, 0))
+    u = spsolve(A=k_lg, b=np.append(f, 0))
 
     # compute error
     err = u[-1] / max(np.absolute(u))
 
     if err > 1e-5:
-        err = "Lagrangian multiplier method error exceeds tolerance of 1e-5."
-        raise RuntimeError(err)
+        msg = "Lagrangian multiplier method error exceeds tolerance of 1e-5."
+        raise RuntimeError(msg)
 
     return u[:-1]
 
@@ -113,22 +130,27 @@ def solve_direct_lagrange(k_lg, f):
 class CustomTimeElapsedColumn(ProgressColumn):
     """Renders time elapsed in milliseconds."""
 
-    def render(self, task: "Task") -> Text:
-        """Show time remaining."""
+    def render(
+        self,
+        task: Task,
+    ) -> Text:
+        """Show time remaining.
 
+        Args:
+            task: Rich progress task
+        """
         elapsed = task.finished_time if task.finished else task.elapsed
 
         if elapsed is None:
             return Text("-:--:--", style="progress.elapsed")
 
-        elapsed_string = "[ {0:.4f} s ]".format(elapsed)
+        elapsed_string = f"[ {elapsed:.4f} s ]"
 
         return Text(elapsed_string, style="progress.elapsed")
 
 
 def create_progress():
     """Returns a Rich Progress class."""
-
     return Progress(
         SpinnerColumn(),
         TextColumn(
