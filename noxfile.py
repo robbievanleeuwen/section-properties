2789c437db3316ab89d914b15f53fcd57096b469
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2023-10-11 15:09:15 +1100

    Build with python 3.11

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,7 @@ except ImportError:
 
 
 package = "sectionproperties"
-python_versions = ["3.9", "3.11", "3.10"]
+python_versions = ["3.11", "3.10", "3.9"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
