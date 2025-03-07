311a9f6acdffe26d5399fc16e5bce560ab7fe435
robbievanleeuwen <robbie.vanleeuwen@gmail.com>
2025-03-07 07:37:49 +0000

    Swap find_holes out for filter_polygons

--- typings/cad_to_shapely/utils.pyi
+++ typings/cad_to_shapely/utils.pyi
@@ -1,3 +1,7 @@
 from shapely.geometry import Polygon
 
 def find_holes(polygons: list[Polygon]) -> Polygon: ...
+def filter_polygons(
+    polygons: list[Polygon],
+    filter_flag: int = ...,
+) -> list[Polygon]: ...
